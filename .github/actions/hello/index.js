dedf213bb27ed19f3706bf3a81a7047821fc25e1
Shreyansh <shreyansh.pandey@simformsolutions.com>
2025-06-24 19:24:18 +0530

    simple action with custom action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,33 @@
+//...see actions/toolkit repo for more imp packages...
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+
+try {
+
+core.debug('Debugging is enabled');
+core.warning('This is a warning message');
+core.console.error('This is an error message');
+
+const name = core.getInput('who_to_greet');
+
+console.log(`Hello ${name}!`);
+
+const time = new Date();
+core.setOutput("time", time.toTimeString());
+
+core.exportVariable("HELLO_TIME", time) //Available in workflow that uses this action
+
+
+//For log groups
+core.startGroup("Logging GitHub context");
+console.log(JSON.stringify(github.context, null, 2));
+core.endGroup();
+
+}
+
+
+catch (error) {
+  core.setFailed(error.message);
+}
