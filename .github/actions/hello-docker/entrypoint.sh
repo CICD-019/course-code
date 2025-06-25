71b9cf1a1bc4c4e9ae267aafd1fc56c9a90e5b84
Shreyansh <shreyansh.pandey@simformsolutions.com>
2025-06-25 11:00:11 +0530

    Simple action with custom Docker Action

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "::debug::Running entrypoint.sh"
 echo "Hello $1"
-echo "INPUT_WHO_TO_GREET": $INPUT_WHO_TO_GREET"
+echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
 echo "HELLO: $HELLO"
-TIME = $(date)
+TIME=$(date)
 echo "time=$TIME" >> $GITHUB_OUTPUT 
 echo "HELLO_TIME=$TIME" >> $GITHUB_ENV
