bc134df93b9d6eb5fe8613ffc663f5edc3d8a1b4
Shreyansh <shreyansh.pandey@simformsolutions.com>
2025-06-25 10:57:48 +0530

    Simple action with custom Docker Action

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -6,6 +6,8 @@ const github = require('@actions/github');
 
 try {
 
+//throw new Error('This is a test error'); 
+// recompile index.js using ncc and upload and wrror willl b catched by setFailed
 core.debug('Debugging is enabled');
 core.warning('This is a warning message');
 core.error('This is an error message');  // Fixed: removed .console
@@ -20,7 +22,7 @@ core.setOutput("time", time.toTimeString());
 core.exportVariable("HELLO_TIME", time) //Available in workflow that uses this action
 
 
-//For log groups
+//For log type groups
 core.startGroup("Logging GitHub context");
 console.log(JSON.stringify(github.context, null, 2));
 core.endGroup();
